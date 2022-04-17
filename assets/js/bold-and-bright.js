1395b9e80ac6f90df91062e12547b1c2518f64f7
Joshua Tapp <jct7bm@virginia.edu>
2022-04-17 01:50:33 -0400

    ran linter and add modified trunk.yaml to ignore minified files

--- assets/js/bold-and-bright.js
+++ assets/js/bold-and-bright.js
@@ -1,14 +1,19 @@
-(function() {
+(function () {
   "use strict"; // Start of use strict
 
-  var mainNav = document.querySelector('#mainNav');
+  var mainNav = document.querySelector("#mainNav");
 
   if (mainNav) {
-
     // Collapse Navbar
-    var collapseNavbar = function() {
-
-      var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    var collapseNavbar = function () {
+      var scrollTop =
+        window.pageYOffset !== undefined
+          ? window.pageYOffset
+          : (
+              document.documentElement ||
+              document.body.parentNode ||
+              document.body
+            ).scrollTop;
 
       if (scrollTop > 100) {
         mainNav.classList.add("navbar-shrink");
@@ -23,16 +28,15 @@
   }
 
   // bageutteBox init
-  if (document.getElementsByClassName('popup-gallery').length > 0) {
-    baguetteBox.run('.popup-gallery', { animation: 'slideIn' });
+  if (document.getElementsByClassName("popup-gallery").length > 0) {
+    baguetteBox.run(".popup-gallery", { animation: "slideIn" });
   }
 
   function initParallax() {
-
-    if (!('requestAnimationFrame' in window)) return;
+    if (!("requestAnimationFrame" in window)) return;
     if (/Mobile|Android/.test(navigator.userAgent)) return;
 
-    var parallaxItems = document.querySelectorAll('[data-bss-parallax]');
+    var parallaxItems = document.querySelectorAll("[data-bss-parallax]");
 
     if (!parallaxItems.length) return;
 
@@ -40,26 +44,28 @@
     var visible = [];
     var scheduled;
 
-    window.addEventListener('scroll', scroll);
-    window.addEventListener('resize', scroll);
+    window.addEventListener("scroll", scroll);
+    window.addEventListener("resize", scroll);
 
     scroll();
 
     function scroll() {
-
       visible.length = 0;
 
       for (var i = 0; i < parallaxItems.length; i++) {
         var rect = parallaxItems[i].getBoundingClientRect();
-        var speed = parseFloat(parallaxItems[i].getAttribute('data-bss-parallax-speed'), 10) || defaultSpeed;
+        var speed =
+          parseFloat(
+            parallaxItems[i].getAttribute("data-bss-parallax-speed"),
+            10
+          ) || defaultSpeed;
 
         if (rect.bottom > 0 && rect.top < window.innerHeight) {
           visible.push({
             speed: speed,
-            node: parallaxItems[i]
+            node: parallaxItems[i],
           });
         }
-
       }
 
       cancelAnimationFrame(scheduled);
@@ -67,21 +73,18 @@
       if (visible.length) {
         scheduled = requestAnimationFrame(update);
       }
-
     }
 
     function update() {
-
       for (var i = 0; i < visible.length; i++) {
         var node = visible[i].node;
         var speed = visible[i].speed;
 
-        node.style.transform = 'translate3d(0, ' + (-window.scrollY * speed) + 'px, 0)';
+        node.style.transform =
+          "translate3d(0, " + -window.scrollY * speed + "px, 0)";
       }
-
     }
   }
 
   initParallax();
 })(); // End of use strict
-
