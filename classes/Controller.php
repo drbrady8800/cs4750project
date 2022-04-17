7320c316457a0ae494758cd49da79f6eaf07fa30
Declan Brady <drbrady8800@gmail.com>
2022-04-17 18:17:36 -0400

    SQL php queries and php transition

---
+++ classes/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+class Controller
+{
+    private $command;
+    private $db; // the idea is give our controller sole access to the database connection
+
+    // contructor
+
+    // controler logic
+
+}
